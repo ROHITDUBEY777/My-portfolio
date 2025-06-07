592c950044e5838478309371e166b055920f297e
ROHIT DUBEY <rohitdubey713010@gmail.com>
2025-06-07 12:43:59 +0530

    New commit

--- src/components/INFO/Skills.js
+++ src/components/INFO/Skills.js
@@ -5,50 +5,50 @@ export const SkillObj = [
     id: "1",
     title: "REACT",
     Text:"A JavaScript library for building user interfaces, especially single-page applications (SPAs). Developed by Facebook, React lets you build reusable UI components and efficiently update the DOM using a virtual DOM.",
-    BackgroundUrl:cardBg,
+    
   },
   {
     id: "2",
     title: "TAILWIND",
     Text:"A utility-first CSS framework that lets you style your web pages directly in your HTML or JSX using predefined class names. It's highly customizable and makes building responsive designs super fast.",
-    BackgroundUrl:"./src/components/assets/Benefits/card.svg",
+    
   },
   {
     id: "3",
     title: "CSS",
     Text:" A JavaScript library for building dynamic user interfaces with reusable components. Makes apps faster and more maintainable.",
-    BackgroundUrl:"./src/components/assets/Benefits/card.svg",
+  
   },
   {
     id: "4",
     title: "JAVASCRIPT",
     Text:"A high-level, versatile programming language used to create interactive and dynamic content on websites. It's one of the core technologies of the web, alongside HTML and CSS, enabling things like animations, form validations, and complex web app logic.",
-    BackgroundUrl:"./src/components/assets/Benefits/card.svg",
+    
   },
   {
     id: "5",
     title: "HTML",
     Text:"The standard language for creating and structuring content on the web. HTML provides the basic building blocks for web pages, such as headings, paragraphs, links, images, and layout containers.",
-    BackgroundUrl:"./src/components/assets/Benefits/card.svg",
+  
   },
   {
     id: "6",
     title: "UIUX",
     Text:"UI/UX Design focuses on creating intuitive and visually appealing digital experiences Figma is a collaborative design tool used to create wireframes, mockups, and interactive prototypes-all in one place, directly in the browser.",
-    BackgroundUrl:"./src/components/assets/Benefits/card.svg",
+  
   },
   {
     id: "7",
     title: "GITHUB",
     Text:"GitHub is a web-based platform used for version control and collaboration. It allows developers to store, track, and manage their code using Git, a distributed version control system.",
-    BackgroundUrl:"./src/components/assets/Benefits/card.svg",
+
   },
   
   {
     id: "8",
     title: "EMAIL JS",
     Text:"EmailJS enables client-side email sending by connecting your form to services like Gmail using prebuilt templates and API keys.",
-    BackgroundUrl:"./src/components/assets/Benefits/card.svg",
+
   }
 
  
