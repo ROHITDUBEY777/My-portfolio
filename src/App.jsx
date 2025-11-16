714394845eb5512a8366c831bf525e9aab9ce7af
Rohit Dubey <rohitdubey713010@gmail.com>
2025-11-16 21:31:49 +0530

    Fixed some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,71 @@
+import React, { useEffect } from "react";
+import Home from "./components/Home";
+import Navbar from "./components/sections/Navbar";
+import About from "./components/sections/About";
+import Projects from "./components/sections/Projects";
+import Resume from "./components/sections/Resume";
+import Footer from "./components/sections/Footer";
+import Contact from "./components/sections/Contact";
+import NewSkills from "./components/sections/NewSkills";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import React from "react"
-import Home from "./components/Home"
-import Navbar from "./components/sections/Navbar"
-import About from "./components/sections/About"
-// import MySkills from "./components/sections/MySkills"
-import Projects from "./components/sections/Projects"
-
-
-import Resume from "./components/sections/Resume"
-import { useEffect } from "react"
-import Footer from "./components/sections/Footer"
-import Contact from "./components/sections/Contact"
-import {BrowserRouter as Router,Routes,Route } from "react-router-dom"
-import NewSkills from "./components/sections/NewSkills"
 import gsap from "gsap";
-import { ScrollSmoother } from "gsap/ScrollSmoother"
-import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollSmoother,ScrollTrigger)
+gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
+
 function App() {
-
-  useEffect(()=>{
-    if(!ScrollSmoother.get()){
-      ScrollSmoother.create({
-        // wrapper:"#smooth-wrapper",
-        content:"#smooth-content",
-        smooth:1,
-        effects:true,
-      })
-    }
-  },[]);
-
-  return (
-    <> 
-       
-      <Router>
-     
-      
- <Navbar />
-       {/* <div id="smooth-wrapper"> */}
-       <div id="smooth-content">
-        
-      
-    <main className="relative min-h-screen  justify-center text-white bg-[var(--background)] overflow-hidden">
-      
-     
-       <Routes>
-       
-
-      <Route path="/" element={        <>
- 
-     
-
-       
-       <Home />
-       <About />
-       <NewSkills />
-       <Projects />
-       <Resume />
-       <Contact />
-       <Footer />
-      
-       </>
-      }/>
-
-      
-       </Routes>
+  useEffect(() => {
+    
+    ScrollTrigger.getAll().forEach((t) => t.kill());
 
     
-    </main>
-      </div>
- 
-        {/* </div> */}
+    const smootherInstance = ScrollSmoother.create({
+      wrapper: "#smooth-wrapper",
+      content: "#smooth-content",
+      smooth: 1.5,
+      effects: true,
+    });
+
+    return () => {
+   
+      smootherInstance.kill();
+      ScrollTrigger.getAll().forEach((t) => t.kill());
+    };
+  }, []);
+
+  return (
+    <>
+      <Router>
+        <Navbar />
+
+      
+        <div id="smooth-wrapper">
+          <div id="smooth-content">
+            <main className="relative min-h-screen text-white bg-[var(--background)] overflow-hidden">
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <>
+                      <Home />
+                      <About />
+                      <NewSkills />
+                      <Projects />
+                      <Resume />
+                      <Contact />
+                      <Footer />
+                    </>
+                  }
+                />
+              </Routes>
+            </main>
+          </div>
+        </div>
       </Router>
     </>
-  ) 
+  );
 }
 
-export default App
+export default App;
